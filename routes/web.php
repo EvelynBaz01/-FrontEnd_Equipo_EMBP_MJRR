d40059aaec60f1ac56a887fc1c8aa657280d1779
Evelyn Michelle Baz Pérez <107454975+EvelynBaz01@users.noreply.github.com>
2022-07-10 19:31:27 -0500

    Se crearon las rutas de las vistas para poder hacer uso de ellas

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('index');
+})->name('inicio');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('quienes_somos');
+
+Route::get('/services', function () {
+    return view('services');
+})->name('servicios');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contacto');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('iniciar_sesion');
+
+Route::get('/registerUser', function () {
+    return view('registerUser');
+})->name('registrar_usu');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('panel_adm');
+
+Route::get('/users', function () {
+    return view('user');
+})->name('lista_usuarios');
+
+Route::get('/Userregister', function () {
+    return view('Userregister');
+})->name('registrar_usuario');
+
+Route::get('/product', function () {
+    return view('product');
+})->name('lista_productos');
+
+Route::get('/registerProd', function () {
+    return view('registerProd');
+})->name('registrar_producto');
+
