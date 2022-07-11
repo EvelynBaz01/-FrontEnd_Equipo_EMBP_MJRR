58888a64c881c07c0f34304a16c31f292a98f392
Evelyn Michelle Baz Pérez <107454975+EvelynBaz01@users.noreply.github.com>
2022-07-10 20:38:46 -0500

    Se corrigieron las rutas del archivo de registerUser donde se muestra un formulario para que el usuario pueda regitrarse

--- resources/views/registerUser.blade.php
+++ resources/views/registerUser.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
     <title>Pizzas Rossely</title>
     <meta charset="utf-8">
@@ -11,35 +11,35 @@
     <link href="https://fonts.googleapis.com/css?family=Nothing+You+Could+Do" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="css/animate.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <link rel="stylesheet" href="css/aos.css">
-    <link rel="stylesheet" href="css/ionicons.min.css">
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="css/jquery.timepicker.css">
-    <link rel="stylesheet" href="css/flaticon.css">
-    <link rel="stylesheet" href="css/icomoon.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/open-iconic-bootstrap.min.css') }}"> 
+    <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/aos.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-datepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/jquery.timepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/icomoon.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
-    <link rel="stylesheet" href="csslogin/style.css">
+    <link rel="stylesheet" href="{{ asset('assets/csslogin/style.css') }}">
   </head>
   <body>
   	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
-		    <a class="navbar-brand" href="index.html"><span class="flaticon-pizza-1 mr-1"></span>Pizzas<br><small>Rossely</small></a>
+		    <a class="navbar-brand" href="{{ route('inicio') }}"><span class="flaticon-pizza-1 mr-1"></span>Pizzas<br><small>Rossely</small></a>
 		    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 		      <span class="oi oi-menu"></span> Menú
 		    </button>
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
-	          <li class="nav-item"><a href="index.html" class="nav-link">Inicio</a></li>
-	          <li class="nav-item"><a href="about.html" class="nav-link">¿Quiénes Somos?</a></li>
-            <li class="nav-item"><a href="services.html" class="nav-link">Servicios</a></li>
-                <li class="nav-item"><a href="contact.html" class="nav-link">Contacto</a></li>
-            <li class="nav-item active"><a href="login.html" class="nav-link">Iniciar Sesión</a></li>
+	          <li class="nav-item"><a href="{{ route('inicio') }}" class="nav-link">Inicio</a></li>
+	          <li class="nav-item"><a href="{{ route('quienes_somos') }}" class="nav-link">¿Quiénes Somos?</a></li>
+            <li class="nav-item"><a href="{{ route('servicios') }}" class="nav-link">Servicios</a></li>
+                <li class="nav-item"><a href="{{ route('contacto') }}" class="nav-link">Contacto</a></li>
+            <li class="nav-item active"><a href="{{ route('iniciar_sesion') }}" class="nav-link">Iniciar Sesión</a></li>
 	        </ul>
 	      </div>
 		  </div>
@@ -51,7 +51,7 @@
         <div class="row justify-content-center">
           <div class="col-md-6 col-lg-4">
             <div class="login-wrap py-5">
-              <div class="img d-flex align-items-center justify-content-center" style="background-image: url(images/Pizza.png);"></div>
+              <div class="img d-flex align-items-center justify-content-center" style="background-image: url(assets/images/Pizza.png);"></div>
                 <h3 class="text-center mb-0">Bienvenido</h3>
                 <p class="text-center">Registrate para poder iniciar sesión</p>
                 <form action="#" class="login-form">
@@ -80,7 +80,7 @@
                       <input type="password" class="form-control" placeholder="Contraseña" required>
                   </div>
                   <div class="form-group"> 
-                      <a type="submit" href="login.html" class="btn form-control btn-primary rounded submit py-3 px-4">
+                      <a type="submit" href="{{ route('iniciar_sesion') }}" class="btn form-control btn-primary rounded submit py-3 px-4">
                           Guardar
                       </a>
                   </div>
@@ -92,21 +92,21 @@
 	  </section>
 
 
-  <script src="js/jquery.min.js"></script>
-  <script src="js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/jquery.easing.1.3.js"></script>
-  <script src="js/jquery.waypoints.min.js"></script>
-  <script src="js/jquery.stellar.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/jquery.animateNumber.min.js"></script>
-  <script src="js/bootstrap-datepicker.js"></script>
-  <script src="js/jquery.timepicker.min.js"></script>
-  <script src="js/scrollax.min.js"></script>
-  <script src="js/main.js"></script>
+  <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+  <script src="{{ asset('assets/js/jquery-migrate-3.0.1.min.js') }}"></script>
+  <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+  <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('assets/js/jquery.easing.1.3.js') }}"></script>
+  <script src="{{ asset('assets/js/jquery.waypoints.min.js') }}"></script>
+  <script src="{{ asset('assets/js/jquery.stellar.min.js') }}"></script>
+  <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+  <script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
+  <script src="{{ asset('assets/js/aos.js') }}"></script>
+  <script src="{{ asset('assets/js/jquery.animateNumber.min.js') }}"></script>
+  <script src="{{ asset('assets/js/bootstrap-datepicker.js') }}"></script>
+  <script src="{{ asset('assets/js/jquery.timepicker.min.js') }}"></script>
+  <script src="{{ asset('assets/js/scrollax.min.js') }}"></script>
+  <script src="{{ asset('assets/js/main.js') }}"></script>
 
   </body>
 </html>
